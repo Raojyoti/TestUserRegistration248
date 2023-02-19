42186b1f987caa8222bbd45a882afb16ad31014b
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-19 20:33:01 +0530

    [Jyoti Rao] Test validation for last name

--- TestUserRegistrationProblems/UserRegistrationTest.cs
+++ TestUserRegistrationProblems/UserRegistrationTest.cs
@@ -7,14 +7,17 @@ namespace TestUserRegistrationProblems
     [TestClass]
     public class UserRegistrationTest
     {
+        /// <summary>
+        ///  UC1-First name starts with Capital and has minimum 3 characters
+        ///  UC2-First name starts with Capital and has minimum 3 characters
+        /// </summary>
         [TestMethod]
-        public void CheckValidationForFirstName()
+        [DataRow("^[A-Z]{1}[a-z]{2,}$","Rahul","Valid")]
+        [DataRow("^[A-Z]{1}[a-z]{2,}$", "Sharma", "Valid")]
+        public void CheckValidationForUserEnterenValue(string pattern, string input, string excepted)
         {
             //AAA
             //Arrange
-            string pattern = "^[A-Z]{1}[a-z]{2,}$";
-            string input = "Rahul";
-            string excepted = "Valid";
             Pattern pat = new Pattern();
             //Act
             string actual = pat.ValidatePattern(input ,pattern);
