0a90d97391157006d7ee0d9c2bc5719058991284
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-19 20:52:01 +0530

    [Jyoti Rao] Test validation for mobile number

--- TestUserRegistrationProblems/UserRegistrationTest.cs
+++ TestUserRegistrationProblems/UserRegistrationTest.cs
@@ -11,11 +11,13 @@ namespace TestUserRegistrationProblems
         ///  UC1-First name starts with Capital and has minimum 3 characters
         ///  UC2-First name starts with Capital and has minimum 3 characters
         ///  UC3-Email has 3 mandatory parts (abc, bl & co) and 2 optional(xyz & in) with precise @ and.positions
+        ///  UC4-Country code follow by space and 10 digit number.
         /// </summary>
         [TestMethod]
         [DataRow("^[A-Z]{1}[a-z]{2,}$","Rahul","Valid")]
         [DataRow("^[A-Z]{1}[a-z]{2,}$", "Sharma", "Valid")]
         [DataRow(@"^([abc]+)(\.[a-z0-9_\+\-]+)?@([bl]+)\.([co]{2,4})(\.[a-z]{2,})?$", "abc.xyz@bl.co.in", "Valid")]
+        [DataRow("^[1-9]{2}?([ ])[0-9]{10}$", "91 9919819801", "Valid")]
         public void CheckValidationForUserEnterenValue(string pattern, string input, string excepted)
         {
             //AAA
--- UserRegistrationProblems/Program.cs
+++ UserRegistrationProblems/Program.cs
@@ -16,6 +16,9 @@ namespace UserRegistrationProblems
                 Console.Clear();
                 Console.WriteLine("Please choose any options");
                 Console.WriteLine("1.Check Validation for First Name\n" +
+                    "2.Check Validation for Last Name\n" +
+                    "3.Check validation for Email Id\n" +
+                    "4.Check validation for Mobile Number\n" +
                     "10.Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -42,14 +45,25 @@ namespace UserRegistrationProblems
                         Console.Write("\nPress any key to continue...... ");
                         Console.ReadLine();
                         break;
-                    case 3:
+                   case 3:
                         Console.Clear();
                         string code2 = Pattern.pattern = @"^([abc]+)(\.[a-z0-9_\+\-]+)?@([bl]+)\.([co]{2,4})(\.[a-z]{2,})?$";//abc.xyz@bl.co.in;
                         Console.WriteLine("Enter emailid want to check for validation:");
                         string emailId = Console.ReadLine();
-                        Console.WriteLine("\nList of valid and Invalid LastName:\n-------------------------------------");
+                        Console.WriteLine("\nList of valid and Invalid emailId:\n-------------------------------------");
                         Pattern pattern2 = new Pattern();
                         pattern2.ValidatePattern(emailId, code2);
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        string code3 = Pattern.pattern = "^[1-9]{2}?([ ])[0-9]{10}$";//91 7805982520
+                        Console.WriteLine("Enter mobile Number want to check for validation:");
+                        string mobileNumber = Console.ReadLine();
+                        Console.WriteLine("\nList of valid and Invalid mobileNumber:\n-------------------------------------");
+                        Pattern pattern3 = new Pattern();
+                        pattern3.ValidatePattern(mobileNumber, code3);
                         Console.Write("\nPress any key to continue...... ");
                         Console.ReadLine();
                         break;
